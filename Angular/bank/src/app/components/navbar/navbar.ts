2a8e17c9c174b2f776c972c06f40292158c0884a
Hossein Simchi <hsimchi74@gmail.com>
2026-05-13 14:10:52 +0330

    added some changes to implement design system

--- Angular/bank/src/app/components/navbar/navbar.ts
+++ Angular/bank/src/app/components/navbar/navbar.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { LOGO_IMG_URL, NAVBAR_ITEMS } from '../../../shared/helpers/constant_values';
+import { LoadImgComponent } from "../load-img/load-img.component";
 
 @Component({
   selector: 'bank-navbar',
-  imports: [],
+  imports: [LoadImgComponent],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar {}
+export class Navbar {
+  LOGO_IMG_URL = LOGO_IMG_URL;
+  NAVBAR_ITEMS = NAVBAR_ITEMS;
+}
---
+++ Angular/bank/src/shared/helpers/constant_values.ts
@@ -0,0 +1,49 @@
+export const LOGO_IMG_URL = 'images/logo.png';
+
+export const NAVBAR_ITEMS = [
+  {
+    id: 1,
+    name: 'Dashboard',
+    icon: 'home',
+  },
+  {
+    id: 2,
+    name: 'Transactions',
+    icon: 'transfer',
+  },
+  {
+    id: 3,
+    name: 'Accounts',
+    icon: 'user',
+  },
+  {
+    id: 4,
+    name: 'Investments',
+    icon: 'economic-investment',
+  },
+  {
+    id: 5,
+    name: 'Credit Cards',
+    icon: 'credit-card',
+  },
+  {
+    id: 6,
+    name: 'Loans',
+    icon: 'loan',
+  },
+  {
+    id: 7,
+    name: 'Services',
+    icon: 'service',
+  },
+  {
+    id: 8,
+    name: 'My Privileges',
+    icon: 'econometrics',
+  },
+  {
+    id: 9,
+    name: 'Setting',
+    icon: 'settings_solid',
+  },
+];
